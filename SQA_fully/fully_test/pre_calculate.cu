6821dd3be0f13399153318e06ddae576bcc40807
test <cs861324@gmail.com>
2020-09-25 00:13:48 +0800

    modeify pre_compute

--- SQA_fully/fully_test/pre_calculate.cu
+++ SQA_fully/fully_test/pre_calculate.cu
@@ -7,13 +7,13 @@
 #include "cublas_v2.h"
 #include "cuda_profiler_api.h"
 
-#define N 2048
+#define N 4096
 #define THREADS 64
-#define TIMES 10
+#define TIMES 1
 
 #define MAX 4294967295.0
 #define STEP 100
-#define M 16
+#define M 64
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert (cudaError_t code, const char *file, int line, bool abort=true)
@@ -60,39 +60,39 @@ __global__ void update_spins (int iter,
 {
     int m = threadIdx.x;
 	 
-	int target_spin = iter - m;
+	int target_spin = (iter - m + N) & (N-1);
     int target_pos = target_spin*M + m;
 
-    if (target_spin >= 0 && target_spin < N) {
-        float delta = 0.;
-        
-        for (int t = (N/THREADS/2)*m; t < (N/THREADS/2)*(m+1); t++)
-            delta += (float)out[t];
-        
-        uint rand = randvals[target_spin];
-		
-        int upper = (m == 0 ? M-1 : m-1);
-		int lower = (m == M-1 ? 0 : m+1);
-		
-        delta = 2.*(float)M*(float)spins[target_pos]*
-				(delta - (float)M*J_perp*(float)(spins[target_spin*M+upper] + spins[target_spin*M+lower]));
-		
-        if ( (-log((float)rand / (float) MAX) / beta) > delta ) {
-			spins[target_pos] = -spins[target_pos];
-		}
-        
-        randvals[target_spin] = rand;
+	float delta = 0.;
+	
+	for (int t = (N/THREADS/2)*m; t < (N/THREADS/2)*(m+1); t++)
+		delta += (float)out[t];
+	
+	uint rand = randvals[target_spin];
+	
+	int upper = (m == 0 ? M-1 : m-1);
+	int lower = (m == M-1 ? 0 : m+1);
+	
+	delta = 2.*(float)M*(float)spins[target_pos]*
+			(delta - (float)M*J_perp*(float)(spins[target_spin*M+upper] + spins[target_spin*M+lower]));
+	
+	if ( (-log((float)rand / (float) MAX) / beta) > delta ) {
+		spins[target_pos] = -spins[target_pos];
 	}
+	
+	randvals[target_spin] = rand;
 }
 
 template <unsigned int blockSize>
 __device__ void warpReduce(volatile int *sdata, unsigned int tid) {
-	if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
-	if (blockSize >= 32) sdata[tid] += sdata[tid + 16];
-	if (blockSize >= 16) sdata[tid] += sdata[tid + 8];
-	if (blockSize >= 8)  sdata[tid] += sdata[tid + 4];
-	if (blockSize >= 4)  sdata[tid] += sdata[tid + 2];
-	if (blockSize >= 2)  sdata[tid] += sdata[tid + 1];
+	for (int m = 0; m < M; m++) {
+		if (blockSize >= 64) sdata[m*THREADS + tid] += sdata[m*THREADS + tid + 32];
+		if (blockSize >= 32) sdata[m*THREADS + tid] += sdata[m*THREADS + tid + 16];
+		if (blockSize >= 16) sdata[m*THREADS + tid] += sdata[m*THREADS + tid + 8];
+		if (blockSize >= 8)  sdata[m*THREADS + tid] += sdata[m*THREADS + tid + 4];
+		if (blockSize >= 4)  sdata[m*THREADS + tid] += sdata[m*THREADS + tid + 2];
+		if (blockSize >= 2)  sdata[m*THREADS + tid] += sdata[m*THREADS + tid + 1];
+	}
 }
 
 template <unsigned int blockSize>
@@ -103,19 +103,49 @@ __global__ void reduce(int iter, char *g_idata, int *g_odata, char *couplings) {
 
     #pragma unroll
 	for (int m = 0; m < M; m++) {
-		int target_spin = iter - m;
-		if (target_spin >= 0 && target_spin < N) {
-			sdata[tid] = g_idata[i*M + m]*couplings[target_spin*N+i] + \
-                         g_idata[(i+blockDim.x)*M + m]*couplings[target_spin*N+i+blockDim.x];
-			__syncthreads();
-			if (blockSize >= 1024) { if (tid < 512) { sdata[tid] += sdata[tid + 512]; } __syncthreads(); }
-			if (blockSize >= 512) { if (tid < 256) { sdata[tid] += sdata[tid + 256]; } __syncthreads(); }
-			if (blockSize >= 256) { if (tid < 128) { sdata[tid] += sdata[tid + 128]; } __syncthreads(); }
-			if (blockSize >= 128) { if (tid < 64) { sdata[tid] += sdata[tid + 64]; } __syncthreads(); }
-			if (tid < 32) warpReduce<blockSize>(sdata, tid);
-			if (tid == 0) g_odata[blockIdx.x + m*(N/THREADS/2)] = sdata[0];
-		}
+		int target_spin = (iter - m + N) & (N-1);
+		sdata[m*THREADS+tid] = g_idata[i*M + m]*couplings[target_spin*N+i] + \
+					 g_idata[(i+blockDim.x)*M + m]*couplings[target_spin*N+i+blockDim.x];
 	}
+	__syncthreads();
+
+	if (blockSize >= 1024) { 
+		if (tid < 512) { 
+			#pragma unroll
+			for (int m = 0; m < M; m++) 
+				sdata[m*THREADS+tid] += sdata[m*THREADS + tid + 512]; 
+		} 
+		__syncthreads(); 
+	}
+	if (blockSize >= 512) { 
+		if (tid < 256) { 
+			#pragma unroll
+			for (int m = 0; m < M; m++) 
+				sdata[m*THREADS+tid] += sdata[m*THREADS + tid + 256]; 
+		} 
+		__syncthreads(); 
+	}
+	if (blockSize >= 256) { 
+		if (tid < 128) { 
+			#pragma unroll
+			for (int m = 0; m < M; m++) 
+				sdata[m*THREADS+tid] += sdata[m*THREADS + tid + 128]; 
+		} 
+		__syncthreads(); 
+	}
+	if (blockSize >= 128) { 
+		if (tid < 64) { 
+			#pragma unroll
+			for (int m = 0; m < M; m++) 
+				sdata[m*THREADS+tid] += sdata[m*THREADS + tid + 64]; 
+		} 
+		__syncthreads(); 
+	}
+	if (tid < 32) warpReduce<blockSize>(sdata, tid);
+
+	// put pack
+	for (int m = 0; m < M; m++)
+		if (tid == 0) g_odata[blockIdx.x + m*(N/THREADS/2)] = sdata[m*THREADS];
 }
 
 void usage () 
@@ -189,7 +219,7 @@ int main (int argc, char *argv[])
             float J_perp = -0.5*log(tanh((Gamma/M)*beta))/beta;
             clock_t begin = clock();
             for (int a = 0; a < M+N-1; a++) {
-				reduce<THREADS><<<grid, block, THREADS*sizeof(int)>>>(a, s_buf, out_buf, couplings_buf);
+				reduce<THREADS><<<grid, block, M*THREADS*sizeof(int)>>>(a, s_buf, out_buf, couplings_buf);
 				update_spins<<<1, M>>>(a, s_buf, couplings_buf, J_perp, beta, randvals, out_buf);
             }
             beta += increase;
@@ -205,7 +235,7 @@ int main (int argc, char *argv[])
             results[t] = E;
             printf("curr: %10lf, energy: %10d\n", curr, E);
         }
-        //printf("Per time step: %10lf, M: %10d, N: %10d\n", curr/(float)STEP, M, N);
+        // printf("Per time step: %10lf, M: %10d, N: %10d\n", curr/(float)STEP, M, N);
         
     }
 
