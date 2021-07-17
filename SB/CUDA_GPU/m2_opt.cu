8057909b4df379a0b1b31fe17e8a5fc74c0b8ea1
drazermega7203 <drazermega@gmail.com>
2021-07-17 16:37:23 +0800

    modified:   m2_opt.cu

--- SB/CUDA_GPU/m2_opt.cu
+++ SB/CUDA_GPU/m2_opt.cu
@@ -6,7 +6,7 @@
 #include <cuda_runtime.h>
 #include "cublas_v2.h"
 #include "cuda_profiler_api.h"
- 
+
 #define N 32768
 #define THREADS 64
 #define TIMES 1
@@ -21,18 +21,21 @@
 #define M 2
 #define STEP 100
 
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert (cudaError_t code, const char *file, int line, bool abort=true)
+#define gpuErrchk(ans)                        \
+    {                                         \
+        gpuAssert((ans), __FILE__, __LINE__); \
+    }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
 {
-   if (code != cudaSuccess)
-   {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
+    if (code != cudaSuccess)
+    {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort)
+            exit(code);
+    }
 }
 
-
-__device__ uint xorshift32 (uint *state)
+__device__ uint xorshift32(uint *state)
 {
     uint x = *state;
     x ^= x << 13;
@@ -42,72 +45,76 @@ __device__ uint xorshift32 (uint *state)
     return x;
 }
 
-__global__ void prepare_points (float *x,
-                                float *y,
-                                uint *randvals) 
+__global__ void prepare_points(float *x,
+                               float *y,
+                               uint *randvals)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    x[idx] = ((xorshift32(&randvals[idx]) / (float) MAX) / 10.0);
-    y[idx] = ((xorshift32(&randvals[idx]) / (float) MAX) / 10.0);
+    x[idx] = ((xorshift32(&randvals[idx]) / (float)MAX) / 10.0);
+    y[idx] = ((xorshift32(&randvals[idx]) / (float)MAX) / 10.0);
 }
 
-__global__ void UpdateTwice (float *x,
-                             float *y,
-				             float amplitude)
+__global__ void UpdateTwice(float *x,
+                            float *y,
+                            float amplitude)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	float lx = x[idx];
-	float ly = y[idx];
+    float lx = x[idx];
+    float ly = y[idx];
 
 #pragma unroll
-	for (int i = 0; i < M; i++) {
-        lx = lx + detune*ly*deltaT;
-        ly = ly-deltaT*(K*lx*lx*lx + (detune - amplitude)*lx);
-	}
+    for (int i = 0; i < M; i++)
+    {
+        lx = lx + detune * ly * deltaT;
+        ly = ly - deltaT * (K * lx * lx * lx + (detune - amplitude) * lx);
+    }
 
-	x[idx] = lx;
-	y[idx] = ly;
+    x[idx] = lx;
+    y[idx] = ly;
 }
 
 void print_energy(float *x, float *x_buf, float *couplings, double curr, int t)
 {
-	gpuErrchk( cudaMemcpy(x, x_buf, N*sizeof(float), cudaMemcpyDeviceToHost) );
-	int E = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = i+1; j < N; j++) {
-			int a = x[i] > 0.0 ? 1 : -1;
-			int b = x[j] > 0.0 ? 1 : -1;
-			E += a*b*couplings[i*N+j];
-		}
-	}
-	printf("%d %lf %d\n", t, curr, -E);
+    gpuErrchk(cudaMemcpy(x, x_buf, N * sizeof(float), cudaMemcpyDeviceToHost));
+    int E = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            int a = x[i] > 0.0 ? 1 : -1;
+            int b = x[j] > 0.0 ? 1 : -1;
+            E += a * b * couplings[i * N + j];
+        }
+    }
+    printf("%d %lf %d\n", t, curr, -E);
 }
 
-void usage () 
+void usage()
 {
     printf("Usage:\n");
     printf("       ./Bifurcation-cuda [spin configuration]\n");
     exit(0);
 }
 
-int main (int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    if (argc != 2) 
+    if (argc != 2)
         usage();
 
     // initialize couplings
     float *couplings, *couplings_buf;
-    couplings = (float*)malloc(N*N*sizeof(float));
-    memset(couplings, '\0', N*N*sizeof(float));
-    gpuErrchk( cudaMalloc(&couplings_buf, N*N*sizeof(float)) );
+    couplings = (float *)malloc(N * N * sizeof(float));
+    memset(couplings, '\0', N * N * sizeof(float));
+    gpuErrchk(cudaMalloc(&couplings_buf, N * N * sizeof(float)));
 
-    // Read couplings file 
+    // Read couplings file
     FILE *instance = fopen(argv[1], "r");
     assert(instance != NULL);
     int a, b, w;
     fscanf(instance, "%d", &a);
-    while (!feof(instance)) {
+    while (!feof(instance))
+    {
         fscanf(instance, "%d%d%d", &a, &b, &w);
         assert(a != b); // not dealing with external field
         couplings[a * N + b] = w;
@@ -115,41 +122,63 @@ int main (int argc, char *argv[])
     }
     fclose(instance);
 
-	// cublas
-	cublasHandle_t handle;
-	cublasCreate(&handle);
+    // cublas
+    cublasHandle_t handle;
+    cublasCreate(&handle);
 
     // copy couplings to target device
-    gpuErrchk( cudaMemcpy(couplings_buf, couplings, N*N*sizeof(float), cudaMemcpyHostToDevice) );
+    gpuErrchk(cudaMemcpy(couplings_buf, couplings, N * N * sizeof(float), cudaMemcpyHostToDevice));
 
     // initialize random number
     uint *randvals, *initRand;
-    gpuErrchk( cudaMalloc(&randvals, N * sizeof(uint)) );
-    initRand = (uint*)malloc(N*sizeof(uint));
+    gpuErrchk(cudaMalloc(&randvals, N * sizeof(uint)));
+    initRand = (uint *)malloc(N * sizeof(uint));
     for (int i = 0; i < N; i++)
         initRand[i] = i;
-    gpuErrchk( cudaMemcpy(randvals, initRand, N*sizeof(uint), cudaMemcpyHostToDevice) );
+    gpuErrchk(cudaMemcpy(randvals, initRand, N * sizeof(uint), cudaMemcpyHostToDevice));
 
     // initialize points
     float *x, *x_buf, *y, *y_buf;
-    x = (float*)malloc(N*sizeof(float));
-    gpuErrchk( cudaMalloc(&x_buf, N*sizeof(float)) );
-    y = (float*)malloc(N*sizeof(float));
-    gpuErrchk( cudaMalloc(&y_buf, N*sizeof(float)) );
+    x = (float *)malloc(N * sizeof(float));
+    gpuErrchk(cudaMalloc(&x_buf, N * sizeof(float)));
+    y = (float *)malloc(N * sizeof(float));
+    gpuErrchk(cudaMalloc(&y_buf, N * sizeof(float)));
 
     // launching kernel
-	float coeff1 = DeltaT * xi;
-	float coeff2 = 1.;
-    dim3 grid(N/THREADS), block(THREADS);
+    float coeff1 = DeltaT * xi;
+    float coeff2 = 1.;
+    dim3 grid(N / THREADS), block(THREADS);
     int results[TIMES] = {0};
-    for (int t = 0; t < TIMES; t++) {
+    for (int t = 0; t < TIMES; t++)
+    {
         prepare_points<<<grid, block>>>(x_buf, y_buf, randvals);
-		double curr = 0.;
-        for (int s = 0; s < STEP; s++) {
-            float amplitude = s/(float)STEP;
-			clock_t begin = clock();
-			UpdateTwice<<<grid, block>>>(x_buf, y_buf, amplitude);
+        double curr = 0.;
+        for (int s = 0; s < STEP; s++)
+        {
+            float amplitude = s / (float)STEP;
+            clock_t begin = clock();
+            float elapsed = 0;
+            cudaEvent_t start, stop;
+
+            gpuErrchk(cudaEventCreate(&start));
+            gpuErrchk(cudaEventCreate(&stop));
+
+            gpuErrchk(cudaEventRecord(start, 0));
+            
+            UpdateTwice<<<grid, block>>>(x_buf, y_buf, amplitude);
 			cublasSgemv(handle, CUBLAS_OP_N, N, N, &coeff1, couplings_buf, N, x_buf, 1, &coeff2, y_buf, 1);
+
+            gpuErrchk(cudaEventRecord(stop, 0));
+            gpuErrchk(cudaEventSynchronize(stop));
+
+            gpuErrchk(cudaEventElapsedTime(&elapsed, start, stop));
+
+            gpuErrchk(cudaEventDestroy(start));
+            gpuErrchk(cudaEventDestroy(stop));
+
+            printf("The elapsed time in gpu was %.2f ms\n", elapsed);
+
+			
 			clock_t end = clock();
 			double duration = (double)(end-begin) / CLOCKS_PER_SEC;
 			curr += duration;
@@ -157,15 +186,17 @@ int main (int argc, char *argv[])
             // print_energy(x, x_buf, couplings, curr, t);
         }
         // Get Result from device
-        gpuErrchk( cudaMemcpy(x, x_buf, N*sizeof(float), cudaMemcpyDeviceToHost) );
+        gpuErrchk(cudaMemcpy(x, x_buf, N * sizeof(float), cudaMemcpyDeviceToHost));
 
         // calculate energy
         int E = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++) {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = i + 1; j < N; j++)
+            {
                 int a = x[i] > 0.0 ? 1 : -1;
                 int b = x[j] > 0.0 ? 1 : -1;
-                E += a*b*couplings[i*N+j];
+                E += a * b * couplings[i * N + j];
             }
         }
         results[t] = -E;
@@ -175,7 +206,7 @@ int main (int argc, char *argv[])
     FILE *output;
     output = fopen("output.txt", "w");
     for (int i = 0; i < TIMES; i++)
-         fprintf(output, "%d\n", results[i]);
+        fprintf(output, "%d\n", results[i]);
     fclose(output);
 
     // Release Objects
